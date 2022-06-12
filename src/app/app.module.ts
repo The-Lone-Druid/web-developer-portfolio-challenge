2ee1b340baa8b623c04147560088ca03186465cb
Zahid Shaikh <Zahid.786shaikh@gmail.com>
2022-06-12 08:34:56 +0530

    * Base setup for the app * Integrated PrimeNG UI library * Integrated BrowserAnimations library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { PartnersComponent } from './components/partners/partners.component';
+import { ProjectViewComponent } from './components/project-view/project-view.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, TopBarComponent, HeroComponent, PartnersComponent, ProjectViewComponent, AboutMeComponent, ContactMeComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
